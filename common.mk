db08b577adf1abebfbfec39799df3db1b26c310c
Anushek Prasal <anushekprasal@gmail.com>
2021-11-28 16:23:11 +0530

    sm8250-common: rro_overlays: Add RRO for OPSoundTuner
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -303,6 +303,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Dolby
+PRODUCT_PACKAGES += \
+    OPSoundTunerOverlay
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
