3846a1535054f5fd9c1aa66ccad206b0b3d963b5
chandu078 <chandudyavanapelli03@gmail.com>
2022-01-02 04:51:34 +0000

    sm8250-common: rro_overlays: Add RRO for OnePlusCameraOverlay
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -428,6 +428,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     oneplus-fwk
 
+# OnePlus Camera
+PRODUCT_PACKAGES += \
+    OnePlusCameraOverlay
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service \
