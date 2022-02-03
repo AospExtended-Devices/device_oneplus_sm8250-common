009597b731fba7a3fb763c63ac985b96bb1eb061
johnmart19 <johnivan19999@gmail.com>
2022-02-03 22:14:41 +0200

    sm8250-common: Add android.hardware.media.c2@1.2 vendor version

--- common.mk
+++ common.mk
@@ -629,7 +629,8 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libmm-omxcore \
-    android.hardware.media.c2@1.0.vendor
+    android.hardware.media.c2@1.0.vendor \
+    android.hardware.media.c2@1.2.vendor
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
