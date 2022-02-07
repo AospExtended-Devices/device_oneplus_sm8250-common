feb13c1d6f469ad9ba2a86a618dda6ef45f0938b
kunalshah <kunalshah912@gmail.com>
2022-02-07 04:59:18 +0000

    remove more unwanted packages
    Signed-off-by: kunalshah <kunalshah912@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := GalleryGoPrebuilt Gallery2 SafetyHubPrebuilt MusicFX AudioFX RecorderPrebuilt Camera2 Music Player Go
+LOCAL_OVERRIDES_PACKAGES := GalleryGoPrebuilt Gallery2 SafetyHubPrebuilt MusicFX AudioFX RecorderPrebuilt Camera2 Music Player Go DuckDuckGo MusicPlayerGO GoogleCameraGo GalleryGo
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
